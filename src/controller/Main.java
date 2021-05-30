5df038da0748786c8abb0b0e93bd54f120838833
kendalqb1 <kendalqb1@gmail.com>
2021-05-29 20:23:16 -0600

    User interface

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -7,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.sql.ResultSet;
 import java.util.Objects;
 
 public class Main extends Application {
