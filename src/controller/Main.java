4c2a083fa99780825b4e7c904c0de45fa20d7b72
kendalqb1 <kendalqb1@gmail.com>
2021-05-29 22:41:40 -0600

    Added font Raleway

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.util.Objects;
@@ -14,14 +15,17 @@ public class Main extends Application {
     public void start(Stage stage) {
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../view/Login.fxml")));
+
             Scene scene = new Scene(root);
             stage.setTitle("Login");
             stage.setResizable(false);
             stage.setScene(scene);
             scene.getStylesheets().add("assets/css/style.css");
+            Font.loadFont(getClass().getResourceAsStream("Raleway-Regular.ttf"), 10);
             stage.getIcons().add(new Image("assets/img/coffe-cup.png"));
             stage.setTitle("StarBuzz Login");
             stage.show();
+
         }
         catch (Exception e) {
             System.out.println(e.getMessage());
