81315de15356454b809eb9c085d74681d82962a7
kendalqb1 <kendalqb1@gmail.com>
2021-06-01 08:39:54 -0600

    Fixing different files

--- src/controller/Login.java
+++ src/controller/Login.java
@@ -1,10 +1,17 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -12,6 +19,9 @@ import java.sql.SQLException;
 public class Login {
 
     Database db = new Database();
+
+    @FXML
+    private AnchorPane anchorPane;
 
     @FXML
     private TextField fieldUsername;
@@ -22,11 +32,29 @@ public class Login {
     @FXML
     private Button btnLogin;
 
+    private int contador = 0;
+
     @FXML
-    void loginManagement() throws SQLException {
+    void loginManagement() throws SQLException, IOException {
         if (!searchUser()) {
             System.out.println("denied");
+            contador++;
+            if(contador == 3) {
+                //TODO: Close window
+                System.out.println("Close window");
+            }
         } else {
+            //TODO: Open dashboard
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/Homepage.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setTitle("Dashboard");
+            stage.setResizable(false);
+            stage.setScene(scene);
+            stage.getIcons().add(new Image("assets/img/coffe-cup.png"));
+            stage.show();
+
             System.out.println("successful");
         }
     }
