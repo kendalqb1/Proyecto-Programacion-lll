c7847fdc8f3cd90e050a5469a25de8ec00530621
kendalqb1 <kendalqb1@gmail.com>
2021-06-01 09:30:16 -0600

    Added decator Beverage classes

---
+++ src/controller/decorator/DarkRoast.java
@@ -0,0 +1,12 @@
+package controller.decorator;
+
+public class DarkRoast extends Beverage {
+    public DarkRoast() {
+        description = "Dark Roast";
+    }
+
+    //TODO: Price DarkRoast
+    public double cost() {
+        return 0;
+    }
+}
---
+++ src/controller/decorator/Espresso.java
@@ -0,0 +1,12 @@
+package controller.decorator;
+
+public class Espresso extends Beverage{
+    public Espresso() {
+        description = "Espresso";
+    }
+
+    //TODO: Price Espresso
+    public double cost() {
+        return 0;
+    }
+}
