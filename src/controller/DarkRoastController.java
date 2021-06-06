9227557461860e639554b9ae9d77257c1059df35
kendalqb1 <kendalqb1@gmail.com>
2021-06-06 14:09:35 -0600

    Complete order

--- src/controller/DarkRoastController.java
+++ src/controller/DarkRoastController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import controller.decorator.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.layout.AnchorPane;
@@ -25,17 +26,52 @@ public class DarkRoastController {
     private CheckBox boxNone;
 
     @FXML
+    void initialize() {
+
+    }
+
+    @FXML
     void closeWindow() {
         ((Stage) anchorPane.getScene().getWindow()).close();
     }
 
     @FXML
     void completeOrder() {
+        Beverage beverage = new DarkRoast();
+        boolean milk = boxMilk.isSelected();
+        boolean whip = boxWhip.isSelected();
+        boolean moka = boxMoka.isSelected();
+        boolean soy = boxSoy.isSelected();
 
+        if (!boxNone.isSelected()) {
+            if (milk) { beverage = new Milk(beverage); }
+            if (whip) { beverage = new Whip(beverage); }
+            if (moka) { beverage = new Moka(beverage); }
+            if (soy) { beverage = new Soy(beverage); }
+        }
+        System.out.println(beverage.getDescription() + " ₡" +  beverage.cost());
+        closeWindow();
+    }
 
+    @FXML
+    void pressedBox() {
+        if(boxNone.isSelected()) {
+            boxMilk.setDisable(true);
+            boxWhip.setDisable(true);
+            boxMoka.setDisable(true);
+            boxSoy.setDisable(true);
+        }
+        else {
+            boxMilk.setDisable(false);
+            boxWhip.setDisable(false);
+            boxMoka.setDisable(false);
+            boxSoy.setDisable(false);
+        }
 
+        boxNone.setDisable(boxMilk.isSelected() || boxWhip.isSelected() || boxMoka.isSelected() || boxSoy.isSelected());
     }
 
 
-
 }
+
+
--- src/controller/HouseBlendController.java
+++ src/controller/HouseBlendController.java
@@ -29,7 +29,6 @@ public class HouseBlendController {
     @FXML
     void initialize() {
 
-
     }
 
     @FXML
