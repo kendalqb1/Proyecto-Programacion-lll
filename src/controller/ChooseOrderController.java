7c6a4201d9c428c1253c36400741ff8f59e89196
kendalqb1 <kendalqb1@gmail.com>
2021-06-06 21:57:20 -0600

    Commit choose order

--- src/controller/ChooseOrderController.java
+++ src/controller/ChooseOrderController.java
@@ -4,14 +4,26 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import model.Checkout;
+import model.Order;
 import model.decorator.DarkRoast;
 import model.decorator.Decaf;
 import model.decorator.Espresso;
 import model.decorator.HouseBlend;
 
 public class ChooseOrderController {
+
+    @FXML
+    private TextField totalPrice;
+
+    @FXML
+    void initialize() {
+
+    }
+
 
     @FXML
     void houseBlendDecorator() {
