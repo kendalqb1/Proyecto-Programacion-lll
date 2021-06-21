ccbee639d3eeca5b14089d2c4a1134a644cf4b67
kendalqb1 <kendalqb1@gmail.com>
2021-06-21 17:47:29 -0600

    Commit 100000000000

---
+++ src/model/DAO/UserDao.java
@@ -0,0 +1,16 @@
+package model.DAO;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class UserDao implements UserDaoInterface {
+    Database db = new Database();
+
+    @Override
+    public ResultSet read() throws SQLException {
+        Statement st = db.getConnection().createStatement();
+        String query = "SELECT * FROM users";
+        return st.executeQuery(query);
+    }
+}
