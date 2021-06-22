3d626ad4107eded4c79933359a17397c841319ef
CHRISTOPHER VALLE PERAZA <christopher.valle.peraza@est.una.ac.cr>
2021-06-21 18:09:35 -0600

    mysql

--- src/controller/Database.java
+++ src/controller/Database.java
@@ -1,9 +1,8 @@
 package controller;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Database {
 
@@ -19,7 +18,40 @@ public class Database {
         }
     }
 
+    public static void writeData(String desc, double amt) {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc-factura", "root", "admin");
+            PreparedStatement pst = connection.prepareStatement("INSERT INTO orders values(?,?,?,?)");
+            pst.setString(1, "0");
+            pst.setString(2, desc);
+            pst.setDouble(3, amt);
+            pst.setString(4, "Pending");
+            pst.executeUpdate();
 
+        }
+        catch (Exception e) {
+            System.out.println("database error");
+        }
+    }
+
+
+
+    public List<String> readDescription() {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc-factura", "root", "admin");
+            PreparedStatement pst = connection.prepareStatement("SELECT * FROM orders where status = ?");
+            pst.setString(4,"Pending");
+
+            List<String> descList = new ArrayList<>();
+            ResultSet rs = pst.executeQuery();
+
+            while(rs.next()){
+                descList.add(rs.getString("description"));
+
+            }return descList;
+        }
+        catch (Exception e) { return null;}
+    }
 
 
 }
